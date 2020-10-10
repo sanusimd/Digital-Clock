5701ffb2c3790d48ff4e6b5385a207c33dfa1c52
SanusiMD <just4deen@yahoo.com>
2020-10-10 23:09:09 +0100

    add functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+function clock() {
+  const date = new Date();
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let midday = "AM";
+
+  //   ADD ZERO TO TIME IF LESS 10
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+
+  // CHECK IF MIDDAY
+  //   if (hours >= 12){
+  //       midday = 'AM'
+  //   }else{
+  //       midday = 'PM'
+  //   }
+
+  midday = hours >= 12 ? "PM" : "AM";
+
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = mins;
+  document.querySelector(".seconds").innerHTML = secs;
+  document.querySelector(".meridan").innerHTML = midday;
+}
+
+setInterval(clock, 100);
