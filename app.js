a08a70d49bf48af0edb43f72ab84175f3a562c7a
SanusiMD <just4deen@yahoo.com>
2020-10-11 15:57:57 +0100

    create alarm section

--- app.js
+++ app.js
@@ -19,11 +19,37 @@ function clock() {
   // CHECK IF MIDDAY
   midday = hours >= 12 ? "PM" : "AM";
 
-//   SELECT ELEMENT 
+  //   SELECT ELEMENT
   document.querySelector(".hours").innerHTML = hours;
   document.querySelector(".minutes").innerHTML = mins;
   document.querySelector(".seconds").innerHTML = secs;
   document.querySelector(".meridan").innerHTML = midday;
 }
 
-setInterval(clock, 100);
+setInterval(clock, 1000);
+
+// Set Alarm
+let seconds = document.querySelector("#secs");
+let minutes = document.querySelector("#mins");
+let hours = document.querySelector("#hours");
+
+// function for adding min and secs options to select element
+function addMinsSecs(id) {
+  let select = id;
+  let mins = 59;
+  for (let i = 0; i <= mins; i++) {
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i);
+  }
+}
+addMinsSecs(seconds);
+addMinsSecs(minutes);
+
+// Function for adding hours options to select element
+function addHour(id) {
+  let select = id;
+  let hours = 12;
+  for (let i = 0; i <= hours; i++) {
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i);
+  }
+}
+addHour(hours);
